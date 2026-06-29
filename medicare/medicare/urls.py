ee345495390dc55fb747214e99f6c922d15834aa
prabhakarsingh2007 <prabhakarkumarsingh578@gmail.com>
2026-06-29 12:45:08 +0530

    feat: initialize project media assets, configure static media serving, and update gitignore settings

--- medicare/medicare/urls.py
+++ medicare/medicare/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('superadmin/', admin.site.urls),
@@ -12,4 +13,6 @@ urlpatterns = [
     path("", include("payments.urls")),
     path("", include("dashboard.urls")),
     path("", include("ehr.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
